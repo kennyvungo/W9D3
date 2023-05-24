634e9b71641ae169c2553cb5e56508fe2970d2a4
kennyvngo <kennyvungo@gmail.com>
2023-05-24 15:20:07 -0700

    worked on API

--- practice-for-ch-js-ajax-twitter-long-practice-main 2/app/javascript/util/api.js
+++ practice-for-ch-js-ajax-twitter-long-practice-main 2/app/javascript/util/api.js
@@ -2,9 +2,26 @@ const csrfToken = document.querySelector("meta[name=csrf-token]").content;
 
 async function customFetch(url, options = {}) {
   options.headers = {
-    // Your code here
+    "X-CSRF-Token": csrfToken,
+    "Content-Type": "application/json",
+    "Accept": "application/json",
     ...options.headers
   };
-
+  
   return await fetch(url, options);
+}
+
+
+export const followUser = (id) => {
+  const options = {
+  method: "POST"
+}
+return customFetch(`/users/${id}/follow`,options)
+}
+
+export const unfollowUser = (id) => {
+  const options = {
+  method: "DESTROY"
+  }
+  return customFetch(`/users/${id}/follow`,options)
 }
